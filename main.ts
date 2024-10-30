f36c4a1cde79242e422177dc1c71e84f0fb6eb57
Zhiqin-Wei <zhiqin.daly.wei@mths.ca>
2024-10-30 10:35:27 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Daly
+ * Created on: Sep 2024
  * This program ...
-*/
+ */
 
-basic.showString('Hello, World!')
+
+
+// variables
+let lightValue: number
+ let lightValuePercentage: number
+
+input.onButtonPressed(Button.A, function () {
+  // input
+  lightValue = input.lightLevel()
+
+  //process
+  lightValuePercentage = (lightValue / 255) * 100
+  lightValuePercentage = Math.round(lightValuePercentage)
+
+  //output 
+  basic.clearScreen()
+  basic.showString('Percentage light is: ' +lightValuePercentage.toString() + "%")
+}) 
